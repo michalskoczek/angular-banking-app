e95fcfe5393b68a2d66eeb6e3318a49f448e7c03
Michał Skoczek <mwskoczek@gmail.com>
2021-09-13 15:49:27 +0200

    Added two postTile to array of postTiles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,24 @@ import { PostTile } from './post-tile';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'start-angular';
+  postTile1: PostTile = {
+    img: 'Obrazek1',
+    title: 'Tytuł 1',
+    category: 'Kategoria 1',
+    descr: 'Skrócony opis 1',
+    date: new Date(),
+    comments: 2
+  }
+
+  postTile2: PostTile = {
+    img: 'Obrazek2',
+    title: 'Tytuł 2',
+    category: 'Kategoria 2',
+    descr: 'Skrócony opis 2',
+    date: new Date(),
+    comments: 5
+  }
+  public readonly articleTilesInfo = [ 
+    this.postTile1, this.postTile2
+  ]
 }
